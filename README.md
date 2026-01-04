303a936e69374e298bdae771232e4e7fed7f22d4
sarah <sarah2049@proton.me>
2026-01-04 15:25:07 +0800

    feat: Add Uniswap, Aerodrome, and Pancake V3 pool query contracts while removing the Counter example and updating the README.

--- README.md
+++ README.md
@@ -1,66 +1,50 @@
-## Foundry
+# Uniswap Ticks Query
 
-**Foundry is a blazing fast, portable and modular toolkit for Ethereum application development written in Rust.**
+Powerful and efficient Solidity contracts designed to query liquidity ticks from Uniswap V3 and its major forks (PancakeSwap V3, Aerodrome V3).
 
-Foundry consists of:
+## Features
 
--   **Forge**: Ethereum testing framework (like Truffle, Hardhat and DappTools).
--   **Cast**: Swiss army knife for interacting with EVM smart contracts, sending transactions and getting chain data.
--   **Anvil**: Local Ethereum node, akin to Ganache, Hardhat Network.
--   **Chisel**: Fast, utilitarian, and verbose solidity REPL.
+- **Multi-Protocol Support**: Compatible with Uniswap V3, PancakeSwap V3, and Aerodrome V3.
+- **Efficient Querying**: Optimized to retrieve multiple liquidity ticks and pool state in a single call.
+- **Gas Optimized**: Uses assembly and packed encoding for minimal gas consumption.
+- **Flexible Data Retrieval**: Supports fetching specific ticks or entire ranges within a word.
 
 ## Documentation
 
-https://book.getfoundry.sh/
+For more information and detailed API documentation, visit [uint256.xyz](https://uint256.xyz).
 
-## Usage
+## Deployment Addresses
+
+The contracts are deployed on **Base Mainnet**:
+
+| Contract | Address | Explorer |
+| :--- | :--- | :--- |
+| **UniswapV3PoolQuery** | `0xDf7acDFaab84FE57c999aEf080749845C97ca038` | [Basescan](https://basescan.org/address/0xdf7acdfaab84fe57c999aEf080749845C97ca038) |
+| **PancakeV3Query** | `0x398FbFe61579090aEcC613a25BdeCffBa8D60313` | [Basescan](https://basescan.org/address/0x398fbfe61579090aecc613a25bdecffba8d60313) |
+| **AerodromeV3Query** | `0xB369A9B58bE84783F47E66c244F79567E36367B1` | [Basescan](https://basescan.org/address/0xb369a9b58be84783f47e66c244f79567e36367b1) |
+
+## Quick Start
+
+### Prerequisites
+
+Ensure you have [Foundry](https://book.getfoundry.sh/getting-started/installation) installed.
 
 ### Build
 
-```shell
-$ forge build
-```
-
-### Test
+Compile the smart contracts:
 
 ```shell
-$ forge test
+forge build
 ```
 
 ### Format
 
-```shell
-$ forge fmt
-```
-
-### Gas Snapshots
+Format the codebase:
 
 ```shell
-$ forge snapshot
+forge fmt
 ```
 
-### Anvil
+## License
 
-```shell
-$ anvil
-```
-
-### Deploy
-
-```shell
-$ forge script script/Counter.s.sol:CounterScript --rpc-url <your_rpc_url> --private-key <your_private_key>
-```
-
-### Cast
-
-```shell
-$ cast <subcommand>
-```
-
-### Help
-
-```shell
-$ forge --help
-$ anvil --help
-$ cast --help
-```
+This project is licensed under the UNLICENSED License.
---
+++ src/AerodromeV3Query.sol
@@ -0,0 +1,278 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity =0.7.6;
+pragma abicoder v2;
+
+interface IUniswapV3PoolState {
+    function liquidity() external view returns (uint128);
+
+    function ticks(
+        int24 tick
+    )
+        external
+        view
+        returns (
+            uint128 liquidityGross,
+            int128 liquidityNet,
+            int128 stakedLiquidityNet,
+            uint256 feeGrowthOutside0X128,
+            uint256 feeGrowthOutside1X128,
+            uint256 rewardGrowthOutsideX128,
+            int56 tickCumulativeOutside,
+            uint160 secondsPerLiquidityOutsideX128,
+            uint32 secondsOutside,
+            bool initialized
+        );
+
+    function tickSpacing() external view returns (int24);
+
+    function fee() external view returns (uint24);
+
+    function tickBitmap(int16 wordPosition) external view returns (uint256);
+
+    function slot0()
+        external
+        view
+        returns (
+            uint160 sqrtPriceX96,
+            int24 tick,
+            uint16 observationIndex,
+            uint16 observationCardinality,
+            uint16 observationCardinalityNext,
+            bool unlocked
+        );
+}
+
+contract AerodromeV3Query {
+    int24 internal constant MIN_TICK = -887272;
+    /// @dev The maximum tick that may be passed to #getSqrtRatioAtTick computed from log base 1.0001 of 2**128
+    int24 internal constant MAX_TICK = -MIN_TICK;
+
+    function getTickLiqs(
+        address[] memory poolAddrs,
+        bytes[] memory tickDatas
+    ) external view returns (bytes[] memory datas) {
+        uint len = poolAddrs.length;
+        datas = new bytes[](len);
+        for (uint i; i < len; ++i) {
+            datas[i] = getTickLiq(poolAddrs[i], tickDatas[i]);
+        }
+    }
+
+    function getTickLiq(
+        address poolAddr,
+        bytes memory tickData
+    ) internal view returns (bytes memory data) {
+        uint len;
+        assembly {
+            len := div(mload(tickData), 0x3)
+        }
+        for (uint i; i < len; ++i) {
+            int24 tick;
+            assembly {
+                tick := mload(add(add(tickData, 0x3), mul(i, 0x3)))
+            }
+            (, int128 liquidityNet, , , , , , , , ) = IUniswapV3PoolState(
+                poolAddr
+            ).ticks(tick);
+            data = abi.encodePacked(data, liquidityNet);
+        }
+    }
+
+    function getPopulatedTicksInWords(
+        bytes[] memory sdatas
+    ) external view returns (bytes[] memory datas) {
+        uint256 len = sdatas.length;
+        datas = new bytes[](len);
+        for (uint256 i; i < len; ++i) {
+            datas[i] = getPopulatedTicksInWord(sdatas[i]);
+        }
+    }
+
+    function getPopulatedTicksInWord(
+        bytes memory sdata
+    ) internal view returns (bytes memory data) {
+        address poolAddr;
+        int24 tickSpacing;
+        uint8 lenAdd;
+        uint8 lenSub;
+        assembly {
+            poolAddr := shr(96, mload(add(add(sdata, 0x20), 0x0)))
+            tickSpacing := mload(add(add(sdata, 0x3), 0x14))
+            lenAdd := mload(add(add(sdata, 0x1), 0x17))
+            lenSub := mload(add(add(sdata, 0x1), 0x18))
+        }
+        (uint160 _sqrtPriceX96, int24 tick, , , , ) = IUniswapV3PoolState(
+            poolAddr
+        ).slot0();
+        uint128 liquidity = IUniswapV3PoolState(poolAddr).liquidity();
+        data = abi.encodePacked(_sqrtPriceX96, liquidity, tick);
+        int24 compressed = tick / tickSpacing;
+        int16 wordPos = int16(compressed >> 8);
+        for (uint i = lenSub; i > 0; --i) {
+            data = abi.encodePacked(
+                data,
+                IUniswapV3PoolState(poolAddr).tickBitmap(wordPos - int16(i))
+            );
+        }
+        data = abi.encodePacked(
+            data,
+            IUniswapV3PoolState(poolAddr).tickBitmap(wordPos)
+        );
+        for (uint i = 1; i < lenAdd; ++i) {
+            data = abi.encodePacked(
+                data,
+                IUniswapV3PoolState(poolAddr).tickBitmap(wordPos + int16(i))
+            );
+        }
+    }
+
+    function getAllTicksInWord(
+        address poolAddr,
+        uint32 words
+    )
+        external
+        view
+        returns (
+            uint160 sqrtPriceX96,
+            uint128 liquidity,
+            int24 tickCur,
+            int24[] memory ticks,
+            int128[] memory liquidityNets
+        )
+    {
+        int16 wordPos;
+        int24 tickSpacing;
+        IUniswapV3PoolState poolState = IUniswapV3PoolState(poolAddr);
+        {
+            (sqrtPriceX96, tickCur, , , , ) = IUniswapV3PoolState(poolAddr)
+                .slot0();
+            liquidity = poolState.liquidity();
+            tickSpacing = poolState.tickSpacing();
+            int24 compressed = tickCur / tickSpacing;
+            wordPos = int16(compressed >> 8);
+        }
+        uint256 numberOfTicks;
+        bytes memory tickdata;
+        bytes memory tickNetdata;
+        {
+            uint256 bitmap;
+            int16 lWordNum = -int16((words >> 16) & uint(0xffff));
+            int16 rWordNum = int16(words & uint(0xffff));
+            for (int16 k = lWordNum; k <= rWordNum; ++k) {
+                int16 pos = wordPos + k;
+                bitmap = poolState.tickBitmap(pos);
+                for (uint256 i; i < 256; ++i) {
+                    if (bitmap & (1 << i) > 0) {
+                        int24 populatedTick = ((int24(pos) << 8) + int24(i)) *
+                            tickSpacing;
+                        (, int128 liquidityNet, , , , , , , , ) = poolState
+                            .ticks(populatedTick);
+                        if (liquidityNet != 0) {
+                            tickdata = abi.encodePacked(
+                                tickdata,
+                                populatedTick
+                            );
+                            tickNetdata = abi.encodePacked(
+                                tickNetdata,
+                                liquidityNet
+                            );
+                            numberOfTicks++;
+                        }
+                    }
+                }
+            }
+        }
+
+        ticks = new int24[](numberOfTicks);
+        liquidityNets = new int128[](numberOfTicks);
+        for (uint256 i; i < numberOfTicks; ++i) {
+            int24 tick;
+            int128 liquidityNet;
+            assembly {
+                let tickPos := mul(0x3, i)
+                let liquidityNetPos := mul(0x10, i)
+                tick := mload(add(add(tickdata, 0x3), tickPos))
+                liquidityNet := mload(
+                    add(add(tickNetdata, 0x10), liquidityNetPos)
+                )
+            }
+            ticks[i] = tick;
+            liquidityNets[i] = liquidityNet;
+        }
+    }
+
+    function getAllTicksInWord2(
+        address poolAddr,
+        uint32 words
+    )
+        external
+        view
+        returns (
+            uint160 sqrtPriceX96,
+            uint128 liquidity,
+            int24 tickCur,
+            int24 tickSpacing,
+            uint24 fee,
+            int24[] memory ticks,
+            int128[] memory liquidityNets,
+            uint128[] memory liquidityGross
+        )
+    {
+        int16 wordPos;
+        IUniswapV3PoolState poolState = IUniswapV3PoolState(poolAddr);
+        {
+            (sqrtPriceX96, tickCur, , , , ) = IUniswapV3PoolState(poolAddr)
+                .slot0();
+            liquidity = poolState.liquidity();
+            tickSpacing = poolState.tickSpacing();
+            fee = poolState.fee();
+            int24 compressed = tickCur / tickSpacing;
+            wordPos = int16(compressed >> 8);
+        }
+
+        int16 lWordNum;
+        int16 rWordNum;
+        if (words > 0) {
+            lWordNum = -int16((words >> 16) & uint(0xffff));
+            rWordNum = int16(words & uint(0xffff));
+        } else {
+            lWordNum = -int16((((tickCur - MIN_TICK) / tickSpacing) >> 8) + 1);
+            rWordNum = int16((((MAX_TICK - tickCur) / tickSpacing) >> 8) + 1);
+        }
+
+        uint256 numberOfTicks;
+        uint256 bitmap;
+        int16 pos;
+        {
+            for (int16 k = lWordNum; k <= rWordNum; ++k) {
+                pos = wordPos + k;
+                bitmap = poolState.tickBitmap(pos);
+                for (uint256 i; i < 256; ++i) {
+                    if (bitmap & (1 << i) > 0) {
+                        numberOfTicks++;
+                    }
+                }
+            }
+        }
+
+        ticks = new int24[](numberOfTicks);
+        liquidityNets = new int128[](numberOfTicks);
+        liquidityGross = new uint128[](numberOfTicks);
+        for (int16 k = lWordNum; k <= rWordNum; ++k) {
+            pos = wordPos + k;
+            bitmap = poolState.tickBitmap(pos);
+            for (uint256 i; i < 256; ++i) {
+                if (bitmap & (1 << i) > 0) {
+                    int24 populatedTick = ((int24(pos) << 8) + int24(i)) *
+                        tickSpacing;
+                    (uint128 lg, int128 liquidityNet, , , , , , , , ) = poolState.ticks(
+                        populatedTick
+                    );
+                    ticks[--numberOfTicks] = populatedTick;
+                    liquidityNets[numberOfTicks] = liquidityNet;
+                    liquidityGross[numberOfTicks] = lg;
+                }
+            }
+        }
+    }
+}
